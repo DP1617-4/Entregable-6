945f8b1621af1b588f9362c5a4a75efe72c92fd8
Alberto <alberto0o095bm@gmail.com>
2016-12-10 21:34:48 +0100

    More displays and fixes

--- L06 - Controllers/Acme-Pad-Thai/src/main/java/controllers/user/RecipeUserController.java
+++ L06 - Controllers/Acme-Pad-Thai/src/main/java/controllers/user/RecipeUserController.java
@@ -4,21 +4,28 @@ package controllers.user;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.IngredientService;
 import services.RecipeService;
 import services.SocialUserService;
 import services.UserService;
-
 import controllers.AbstractController;
+import domain.Ingredient;
+import domain.Quantity;
 import domain.Recipe;
+import domain.Step;
 import domain.User;
+import forms.AddIngredient;
+import forms.AddPicture;
+import forms.FilterString;
 
 
 @Controller
@@ -49,6 +56,7 @@ public class RecipeUserController extends AbstractController {
 		
 		ModelAndView result;
 		Collection<Recipe> recipes;
+		FilterString filter = new FilterString();
 		
 		User u = userService.findByPrincipal();
 
@@ -57,9 +65,47 @@ public class RecipeUserController extends AbstractController {
 		result = new ModelAndView("recipe/list");
 		result.addObject("requestURI", "recipe/user/listOwn.do");
 		result.addObject("recipes", recipes);
+ 		result.addObject("filterString", filter);
 		
 		return result;
 	}
 
+	
+	@RequestMapping(value = "/addPicture", method = RequestMethod.POST, params = "addImage")
+	public ModelAndView filter(@Valid AddPicture addPicture, BindingResult binding) {
+		
+		ModelAndView result;
+		Recipe recipe = recipeService.findOne(addPicture.getId());
+		String picture= addPicture.getPicture();
+		
+		if (binding.hasErrors()) {
+			result = new ModelAndView("redirect:/recipe/display.do?recipeId="+addPicture.getPicture());
+		} else {
+
+				try {
+					recipe.getPictures().add(picture);
+					Collection<Quantity> quantities = recipe.getQuantities();
+					Collection<Step> steps = recipe.getSteps();
+					addPicture = new AddPicture();
+					AddIngredient addIngredient = new AddIngredient();
+					Collection<Ingredient> ingredientlist = ingredientService.findAllNotDeleted();
+					
+					result = new ModelAndView("recipe/display");
+					result.addObject("recipe", recipe);
+					result.addObject("ingredients", ingredientlist );
+					result.addObject("quantities", quantities );
+					result.addObject("steps", steps );
+					result.addObject("addIngredient", addIngredient);
+					result.addObject("addPicture", addPicture);
+					
+					
+					
+				} catch (Throwable oops) {
+					result = new ModelAndView("redirect:/recipe/display.do?recipeId="+addPicture.getPicture());			
+			}
+		}
+			
+		return result;
+	}
 	
 }
